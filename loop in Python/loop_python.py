2204b0203c761f2b76bb224cbf5bc824e047528b
zbari023 <barry450643@gmail.com>
2023-03-14 22:48:07 +0100

    first coding in task

---
+++ loop in Python/loop_python.py
@@ -0,0 +1,12 @@
+# Print numbers from 0 to 10 using while
+i = 0
+while i <= 10:
+    print(i)
+    i +=1
+
+# Print numbers from 0 to 1o using for
+# Stop the loop if the number = 5
+for j in range(11):
+    if j ==5:
+        break
+    print(j)
