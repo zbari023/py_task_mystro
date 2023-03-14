e2c25d923de3fd7194c0d67a959f2a4e39ca2ac0
zbari023 <barry450643@gmail.com>
2023-03-14 22:25:21 +0100

    new in task

---
+++ data types_task/data_types_task.py
@@ -0,0 +1,22 @@
+# With is the differences between ‘ ’ “ ” ‘’’ ‘’’
+# I did'n seen any differenz between it
+
+# Create a string with the name ‘mystero’
+strName = 'Mystro'                             # Mystro
+print(strName)
+# Make the string letters capital
+print(strName.upper())                         # MYSTRO
+# Create a list of values from 10 to 20
+listName = [10,11,12,13,14,15,16,17,18,19,20]
+print(listName)                                # [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+# Add 30 to the end of the list
+listName.append(30)
+print(listName)                                # [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 30]
+# Add 40 as the second element of the list
+listName.insert(1,40)
+print(listName)                                # [10, 40, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 30]
+# Print how many elements in the list
+print(len(listName))                           # 13
+# replace the second element in the list with 100
+listName[1] = 100
+print(listName)                                # [10, 100, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 30]
